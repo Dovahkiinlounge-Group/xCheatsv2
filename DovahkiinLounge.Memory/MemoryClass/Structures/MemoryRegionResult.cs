d740731e6c8dfb6d3db3a102303b1da2651d673b
Aaliyah Priester <reneehopeblasey@outlook.de>
2024-03-07 17:06:52 +0100

    Updated The Memory Lib

---
+++ DovahkiinLounge.Memory/MemoryClass/Structures/MemoryRegionResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dovahkiin.Memory.mem
+{
+
+    /// <summary>
+    /// AoB scan information.
+    /// </summary>
+    struct MemoryRegionResult
+    {
+        public UIntPtr CurrentBaseAddress { get; set; }
+        public long RegionSize { get; set; }
+        public UIntPtr RegionBase { get; set; }
+
+    }
+}
---
+++ DovahkiinLounge.Memory/MemoryClass/Structures/Process.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Dovahkiin.Memory.mem
+{
+    /// <summary>
+    /// Information about the opened process.
+    /// </summary>
+    public class Proc
+    {
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+        public Process Process { get; set; }
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+        public IntPtr Handle { get; set; }
+        public bool Is64Bit { get; set; }
+        //public ConcurrentDictionary<string, IntPtr> Modules { get; set; } // Use mProc.Process.Modules instead
+#pragma warning disable CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+        public ProcessModule MainModule { get; set; }
+#pragma warning restore CS8618 // Ein Non-Nullable-Feld muss beim Beenden des Konstruktors einen Wert ungleich NULL enthalten. Erwägen Sie die Deklaration als Nullable.
+    }
+}
